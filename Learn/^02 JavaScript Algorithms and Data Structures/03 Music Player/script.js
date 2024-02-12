13dfca56e6d3a3ad55e60747432fad87cb43f114
AMESocker <aesocher@yahoo.com>
2024-02-12 12:11:41 -0800

    Completed 2.3.52

--- Learn/^02 JavaScript Algorithms and Data Structures/03 Music Player/script.js
+++ Learn/^02 JavaScript Algorithms and Data Structures/03 Music Player/script.js
@@ -107,6 +107,30 @@ const pauseSong = () => {
   audio.pause();
 };
 
+const playNextSong = () => {
+  if(userData?.currentSong===null){
+    playSong(userData?.songs[0].id)
+  }else{
+    const currentSongIndex = getCurrentSongIndex();
+    const nextSong = userData?.songs[currentSongIndex + 1];
+    playSong(nextSong.id)
+  }
+}
+
+const playPreviousSong = () => {
+  if(userData?.currentSong === null){
+    return
+  }else{
+    const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex - 1];
+    playSong(previousSong.id)
+  }
+};
+
+const highlightCurrentSong = () => {
+  const playlistSongElements = document.querySelectorAll('.playlist-song')
+}
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
@@ -129,7 +153,9 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML
 };
 
-const getCurrentSongIndex = () => {};
+const getCurrentSongIndex = () => {
+  return userData?.songs.indexOf(userData?.currentSong)
+};
 
 playButton.addEventListener('click',()=>{
   if(userData?.currentSong === null){
@@ -140,6 +166,10 @@ playButton.addEventListener('click',()=>{
 });
 
 pauseButton.addEventListener('click',pauseSong);
+
+nextButton.addEventListener('click',playNextSong);
+
+previousButton.addEventListener('click',playPreviousSong);
 
 //----Sort Method----
 userData?.songs.sort((a, b) => {
