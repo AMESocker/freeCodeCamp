2a8a20cb1ce50deee5ca4f14c367c0d196adc2f7
AMESocker <aesocher@yahoo.com>
2024-02-14 10:31:57 -0800

    Completed 2.6.11

--- Learn/^02 JavaScript Algorithms and Data Structures/06 Todo App/script.js
+++ Learn/^02 JavaScript Algorithms and Data Structures/06 Todo App/script.js
@@ -12,11 +12,23 @@ const descriptionInput = document.getElementById("description-input");
 //? 'LocalStorage' is a web storage feature of JavaScript that lets you persist data by storing the data as a key:value pair.
 const taskData = []; //? This array will store all the tasks along with their associated data, including title, due date, and description. This storage will enable you to keep track of tasks, display them on the page, and save them to 'localStorage'.
 let currentTask = {}; //? This variable will be used to track the state when editing and discarding tasks.
-//? A method to use with the 'classList' property is the 'toggle' method.The toggle method will add the class if it is not present on the element, and remove the class if it is present on the element.
+//----opening and closing the modal----
 openTaskFormBtn.addEventListener('click',()=>
-  taskForm.classList.toggle('hidden')
+  taskForm.classList.toggle('hidden')//? A method to use with the 'classList' property is the 'toggle' method.The toggle method will add the class if it is not present on the element, and remove the class if it is present on the element.
 );
 //? The HTML 'dialog' element has a 'showModal()' method that can be used to display a modal dialog box on a web page.
-closeTaskFormBtn.addEventListener('click',()=>
+closeTaskFormBtn.addEventListener('click',()=>{
 confirmCloseDialog.showModal()
-);
+});
+//? The HTML dialog element has a close() method that can be used to close a modal dialog box on a web page.
+cancelBtn.addEventListener('click',()=> confirmCloseDialog.close());
+
+discardBtn.addEventListener('click',()=>{
+  confirmCloseDialog.close();
+  taskForm.classList.toggle('hidden');
+});
+//----get the values from the input fields----
+taskForm.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  const dataArrIndex = taskData.findIndex((item)=>item===currentTask.id);//? The 'findIndex()' array method finds and returns the index of the first element in an array that meets the criteria specified by a provided testing function. If no such element is found, the method returns '-1'.
+})
