6346801cd244f9ba41c1525c858e29896ae85970
AMESocker <aesocher@yahoo.com>
2024-02-15 19:05:01 -0800

    Completed 2.7.12

--- Learn/^02 JavaScript Algorithms and Data Structures/07 Decimal to Binary Converter/script.js
+++ Learn/^02 JavaScript Algorithms and Data Structures/07 Decimal to Binary Converter/script.js
@@ -4,9 +4,15 @@ const result = document.getElementById('result');
 //? Recursion is a programming concept where a function calls itself. This can reduce a complex problem into simpler sub-problems, until they become straightforward to solve. In this project, you’ll build a decimal-to-binary converter using JavaScript. You’ll learn the fundamental concepts of recursion, explore the call stack, and build out a visual representation of the recursion process through an animation.
 const checkUserInput = () => {
   console.log(numberInput.value)
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+
+  }
 };
 
 convertBtn.addEventListener('click',checkUserInput);
+
 numberInput.addEventListener('keydown',(event)=>{
-  if(event.key === 'Enter'){}
+  if(event.key === 'Enter'){
+    checkUserInput()
+  }
 });
