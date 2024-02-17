1577565081c34f12a1f7e008b67c19189ce383cd
AMESocker <aesocher@yahoo.com>
2024-02-17 09:56:25 -0800

    Completed 2.8.21

--- Learn/^02 JavaScript Algorithms and Data Structures/08 Number Sorter/script.js
+++ Learn/^02 JavaScript Algorithms and Data Structures/08 Number Sorter/script.js
@@ -7,8 +7,9 @@ const sortInputArray = (event) =>{
     ...document.getElementsByClassName('values-dropdown')
   ].map((dropdown)=>Number(dropdown.value));
 
-updateUI(inputValues)
+const sortedValues = bubbleSort(inputValues);
 
+updateUI(sortedValues)
 };
 
 const updateUI = (array = []) => {
@@ -17,5 +18,20 @@ const updateUI = (array = []) => {
     outputValueNode.innerText = num
   })
 };
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      console.log(array, array[j], array[j + 1]);
+      if (array[j] > array[j + 1]) { 
+        console.log('j > j+1')
+        let temp = array[j];
+        array[j] = array[j + 1]
+        array[j + 1] = temp
+      }
+    }
+  }
+  return array
+}
 
 sortButton.addEventListener('click',sortInputArray);
