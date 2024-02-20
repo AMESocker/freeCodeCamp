b82b1d9f63457aea396189cd0f66eef53b7dfe07
AMESocker <aesocher@yahoo.com>
2024-02-20 09:56:42 -0800

    Completed 2.10.8

---
+++ Learn/^02 JavaScript Algorithms and Data Structures/10 Spreadsheet/script.js
@@ -0,0 +1,37 @@
+const range = (start, end) => Array(end-start+1).fill(start)
+
+window.onload = () => {
+  const container = document.getElementById('container');
+  const createLabel = (name) => {
+    const label = document.createElement('div');
+    label.className = 'label';
+    label.textContent = name;
+    container.appendChild(label)
+  }
+}
+
+//^ Functional Programming is a popular approach to software development. In Functional Programming, developers organize code into smaller functions, then combine those functions to build complex programs.
+
+//^In this spreadsheet application project, you'll learn about parsing and evaluating mathematical expressions, implementing spreadsheet functions, handling cell references, and creating interactive web interfaces. You'll learn how to dynamically update the page based on user input.
+
+//^This project will cover concepts like the map() method, find() method, parseInt(), the includes() method.
+
+//?1 Your project starts with a basic HTML container and some corresponding CSS. Your first task will be to programmatically generate the cells for your spreadsheet.
+
+//?1 The global window object represents the browser window (or tab). It has an onload property which allows you to define behavior when the window has loaded the entire page, including stylesheets and scripts.
+
+//?2 Functions are ideal for reusable logic. When a function itself needs to reuse logic, you can declare a nested function to handle that logic. Here is an example of a nested function:
+/*
+const outer = () => {
+  const inner = () => {
+  };
+};
+*/
+
+//
+//!
+//?
+//*
+//^
+//&
+//~
