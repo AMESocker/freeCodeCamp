076cafebdfd9cb72983e569b66840e25a36bcce0
AMESocker <aesocher@yahoo.com>
2024-02-21 10:56:45 -0800

    Completed 2.10.65

--- Learn/^02 JavaScript Algorithms and Data Structures/10 Spreadsheet/script.js
+++ Learn/^02 JavaScript Algorithms and Data Structures/10 Spreadsheet/script.js
@@ -1,3 +1,7 @@
+const infixToFunction = {
+  "+": (x, y) => x + y,
+}
+
 const isEven = (num) => num % 2 === 0
 const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
 const average = (nums)=>sum(nums)/nums.length;
@@ -20,16 +24,16 @@ const spreadsheetFunctions = {
 const range = (start, end) => Array(end-start+1).fill(start).map((element,index)=>element+index)
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map((code)=>String.fromCharCode(code));
 
-const evalFormula = (x, cells)=>{
+const evalFormula = (x, cells) => {
   const idToText = id => cells.find(cell => cell.id === id).value;
   const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/ig;//?36,37
-  const rangeFromString = (num1,num2)=> range(parseInt(num1),parseInt(num2));
+  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
   const elemValue = num => character => idToText(character + num);
-  const addCharacters = character1 => character2 => num => charRange(character1,character2).map(elemValue(num)) 
-  const rangeExpanded = x.replace(rangeRegex,(_match,char1, num1, char2, num2)=>rangeFromString(num1,num2).map(addCharacters(char1)(char2)));
+  const addCharacters = character1 => character2 => num => charRange(character1, character2).map(elemValue(num))
+  const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
   const cellRegex = /[A-J][1-9][0-9]?/ig;
-  const cellExpanded = rangeExpanded.replace(cellRegex,(match)=>{})
-}//?45,51,59,60
+  const cellExpanded = rangeExpanded.replace(cellRegex, (match) => idToText(match.toUpperCase()))
+} //?45,51,59,60
 
 window.onload = () => {
   const container = document.getElementById('container');
--- Learn/^02 JavaScript Algorithms and Data Structures/Cert Project/01 Palindrome Checker (Cert Projrect)/script.js
+++ Learn/^02 JavaScript Algorithms and Data Structures/Cert Project/01 Palindrome Checker (Cert Projrect)/script.js
@@ -13,6 +13,8 @@ checkButton.addEventListener('click', () => {
 
 const palCheck = (input) => {
   const lowercase = input.toLowerCase()
+  const half = lowercase.length/2
+  const inHalf = lowercase.split(lowercase[half])
   console.log(lowercase)
   result.innerText = `${input} is a palindrome`
 }
