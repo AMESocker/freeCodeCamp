994932b6ac0b9996233eddd6e861258c555fbcaf
AMESocker <aesocher@yahoo.com>
2024-02-21 10:26:50 -0800

    Completed 2.10.62

--- Learn/^02 JavaScript Algorithms and Data Structures/10 Spreadsheet/script.js
+++ Learn/^02 JavaScript Algorithms and Data Structures/10 Spreadsheet/script.js
@@ -24,14 +24,12 @@ const evalFormula = (x, cells)=>{
   const idToText = id => cells.find(cell => cell.id === id).value;
   const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/ig;//?36,37
   const rangeFromString = (num1,num2)=> range(parseInt(num1),parseInt(num2));
-  const elemValue = (num) => {
-    const inner = (character)=>{
-      return idToText(character + num)
-    }
-    return inner
-  }
-  const addCharacters = character1 => character2 => num => {} //?45
-}
+  const elemValue = num => character => idToText(character + num);
+  const addCharacters = character1 => character2 => num => charRange(character1,character2).map(elemValue(num)) 
+  const rangeExpanded = x.replace(rangeRegex,(_match,char1, num1, char2, num2)=>rangeFromString(num1,num2).map(addCharacters(char1)(char2)));
+  const cellRegex = /[A-J][1-9][0-9]?/ig;
+  const cellExpanded = rangeExpanded.replace(cellRegex,(match)=>{})
+}//?45,51,59,60
 
 window.onload = () => {
   const container = document.getElementById('container');
@@ -109,6 +107,19 @@ const innerOne = elemValue(1);
 const final = innerOne("A");
 */
 //?45 'innerOne' would be your inner function, with num set to 1, and 'final' would have the value of the cell with the id of A1. This is possible because functions have access to all variables declared at their creation. This is called closure.
+
+//?51 You can pass a function reference as a callback parameter. A function reference is a function name without the parentheses. For example:
+/*
+const myFunc = (val) => `value: ${val}`;
+const array = [1, 2, 3];
+const newArray = array.map(myFunc);
+*/
+//?51 The .map() method here will call the myFunc function, passing the same arguments that a .map() callback takes. The first argument is the value of the array at the current iteration, so newArray would be [value: 1, value: 2, value: 3].
+
+//?59 Your addCharacters(char1) is also returning a function, which returns another function. You need to make another function call to access that innermost function reference for the .map() callback. JavaScript allows you to immediately invoke returned functions:
+//myFunc(1)("hi");
+
+//?60 You'll notice that you are not using your match parameter. In JavaScript, it is common convention to prefix an unused parameter with an underscore _. You could also leave the parameter empty like so: (, char1) but it is often clearer to name the parameter for future readability.
 //
 //!
 //?
