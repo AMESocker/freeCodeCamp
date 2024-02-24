c7d7f8404d98afd4b9044f399464355c88dd0974
AMESocker <aesocher@yahoo.com>
2024-02-23 18:30:35 -0800

    Completed 2.13.6

---
+++ Learn/^02 JavaScript Algorithms and Data Structures/13 Platformer Game/script.js
@@ -0,0 +1,19 @@
+const startBtn = document.getElementById('start-btn');
+const canvas = document.getElementById('canvas');
+const startScreen = document.querySelector('.start-screen');
+const checkpointScreen = document.querySelector('.checkpoint-screen');
+const checkpointMessage = document.querySelector('.checkpoint-screen > p');
+
+const ctx = canvas.getContext("2d");
+canvas.width = innerWidth
+//?Coding a game is a great way to grasp fundamental programming principles, while also creating an interactive gaming experience.
+
+//?In this platformer game project, you'll continue to learn about classes, objects, inheritance, and encapsulation. You'll also learn how to design and organize game elements efficiently and gain insights into problem-solving and code reusability.
+
+//?4 Before you can begin building out the functionality for the game, you will need to set up the ability to add 2D graphics.
+
+//?4 The Canvas API can be used to create graphics in games using JavaScript and the HTML canvas element.
+
+//?4 You will need to use the getContext method which will provide the context for where the graphics will be rendered.
+
+//canvas.getContext("2d");
