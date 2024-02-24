a5d361db8fef201a60df51ea2d3ad6fa1cac3cbb
AMESocker <aesocher@yahoo.com>
2024-02-24 09:23:39 -0800

    Completed 2.13.18

--- Learn/^02 JavaScript Algorithms and Data Structures/13 Platformer Game/script.js
+++ Learn/^02 JavaScript Algorithms and Data Structures/13 Platformer Game/script.js
@@ -5,7 +5,30 @@ const checkpointScreen = document.querySelector('.checkpoint-screen');
 const checkpointMessage = document.querySelector('.checkpoint-screen > p');
 
 const ctx = canvas.getContext("2d");
-canvas.width = innerWidth
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+const gravity = 0.5
+let isCheckpointCollisionDetectionActive = true
+
+class Player {
+  constructor() {
+    this.position = {
+      x:10,
+      y:400
+    };
+    this.velocity = {
+      x:0,
+      y:0
+    };
+    this.width = 40;
+    this.height = 40;
+  }
+
+  draw(){
+    ctx.fillStyle = '#99c9ff';
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
 //?Coding a game is a great way to grasp fundamental programming principles, while also creating an interactive gaming experience.
 
 //?In this platformer game project, you'll continue to learn about classes, objects, inheritance, and encapsulation. You'll also learn how to design and organize game elements efficiently and gain insights into problem-solving and code reusability.
