5eb4d00e6fbc8d4cdd4675a940262dd8b6023366
AMESocker <aesocher@yahoo.com>
2024-02-26 08:26:20 -0800

    Completed 2.13.30

--- Learn/^02 JavaScript Algorithms and Data Structures/13 Platformer Game/script.js
+++ Learn/^02 JavaScript Algorithms and Data Structures/13 Platformer Game/script.js
@@ -3,31 +3,53 @@ const canvas = document.getElementById('canvas');
 const startScreen = document.querySelector('.start-screen');
 const checkpointScreen = document.querySelector('.checkpoint-screen');
 const checkpointMessage = document.querySelector('.checkpoint-screen > p');
-
 const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 const gravity = 0.5
 let isCheckpointCollisionDetectionActive = true
 
+const proportionalSize = (size) => {
+  return innerHeight < 500
+    ? Math.ceil((size / 500) * innerHeight)
+    : size
+}
+
 class Player {
   constructor() {
     this.position = {
-      x:10,
-      y:400
+      x: 10,
+      y: 400
     };
     this.velocity = {
-      x:0,
-      y:0
+      x: 0,
+      y: 0
     };
     this.width = 40;
     this.height = 40;
   }
-
-  draw(){
+  draw() {
     ctx.fillStyle = '#99c9ff';
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
+
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      if (this.position.y < 0) {
+        this.position.y = 0;
+        this.velocity.y = gravity;
+      }
+      this.velocity.y += gravity;
+    }else{
+      this.velocity.y = 0
+    }
+  }
+
+
 }
 //?Coding a game is a great way to grasp fundamental programming principles, while also creating an interactive gaming experience.
 
