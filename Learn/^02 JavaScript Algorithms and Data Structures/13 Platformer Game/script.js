0ce51db42e3a37def2f2b76614b1c5a8e769d7da
AMESocker <aesocher@yahoo.com>
2024-02-27 09:22:47 -0800

    Completed 2.13.107

--- Learn/^02 JavaScript Algorithms and Data Structures/13 Platformer Game/script.js
+++ Learn/^02 JavaScript Algorithms and Data Structures/13 Platformer Game/script.js
@@ -74,6 +74,29 @@ class Platform {
   }
 }
 
+class CheckPoint {
+  constructor(x, y, z) {
+    this.position = {
+      x,
+      y,
+    };
+    this.width = proportionalSize(40);
+    this.height = proportionalSize(70);
+    this.claimed =false;
+  };
+
+  draw(){
+    ctx.fillStyle = '#f1be32'
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  claim(){
+    this.width = 0
+    this.height = 0
+    this.position.y = Infinity
+    this.claimed = true
+  }
+};
+
 const player = new Player();
 
 const platformPositions = [
@@ -95,11 +118,21 @@ const platforms = platformPositions.map(
   (platform) => new Platform(platform.x, platform.y)
 );
 
+const checkpointPositions = [
+  {x: 1170, y: proportionalSize(80), z: 1},
+  {x: 2900, y: proportionalSize(330), z: 2 },
+  {x: 4800, y: proportionalSize(80), z: 3 },
+]
+
+const checkpoints = checkpointPositions.map((checkpoint)=>new CheckPoint(checkpoint.x, checkpoint.y, checkpoint.z) )
+
 const animate = () => {
   requestAnimationFrame(animate)
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  platforms.forEach((platform) => platform.draw())
+  platforms.forEach((platform) => platform.draw());
+
+  checkpoints.forEach((checkpoint)=>{checkpoint.draw()});
 
   player.update();
 
@@ -113,10 +146,16 @@ const animate = () => {
       platforms.forEach((platform) => {
         platform.position.x -= 5;
       });
+
+      checkpoints.forEach((checkpoint)=>{checkpoint.position.x -= 5})
+
     } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
       platforms.forEach((platform) => {
         platform.position.x += 5;
       });
+
+      checkpoints.forEach((checkpoint)=>{checkpoint.position.x += 5})
+
     }
   }
 
@@ -189,6 +228,11 @@ const startGame = () => {
   canvas.style.display = 'block';
   startScreen.style.display = 'none';
   animate()
+}
+
+const showCheckpointScreen = (msg)=>{
+  checkpointScreen.style.display = 'block';
+  checkpointMessage.textContent = msg
 }
 
 startBtn.addEventListener('click', startGame)
