f703968fc8a34867ff128bbe448e28f6c86b669e
AMESocker <aesocher@yahoo.com>
2024-02-29 14:20:51 -0800

    Completed 2.16.45

--- Learn/^02 JavaScript Algorithms and Data Structures/16 Forum Leaderboard/script.js
+++ Learn/^02 JavaScript Algorithms and Data Structures/16 Forum Leaderboard/script.js
@@ -5,6 +5,52 @@ const avatarUrl = 'https://sea1.discourse-cdn.com/freecodecamp';
 
 const postsContainer = document.getElementById('posts-container');
 
+const allCategories = {
+  299: { category: 'Career Advice', className: 'career' },
+  409: { category: 'Project Feedback',  className:'feedback' },
+  417: { category: 'freeCodeCamp Support', className: 'support' },
+  421: { category: "JavaScript", className: "javascript" },
+  423: { category: "HTML - CSS", className: "html-css" },
+  424: { category: "Python", className: "python" },
+  432: { category: "You Can Do This!", className: "motivation" },
+  560: { category: "Backend Development", className: "backend" },
+}
+
+const forumCategory = (id) => {
+  let selectedCategory = {};
+  if(allCategories.hasOwnProperty(id)){}
+}
+
+const timeAgo = (time) => {
+  const currentTime = new Date();
+  const lastPost = new Date(time);
+
+  const timeDifference = currentTime - lastPost;
+  const msPerMinute = 1000*60;
+
+  const minutesAgo = Math.floor(timeDifference / msPerMinute);
+  const hoursAgo = Math.floor(minutesAgo / 60);
+  const daysAgo = Math.floor(hoursAgo/24);
+
+  if(minutesAgo<60){
+    return `${minutesAgo}m ago`
+  }
+
+  if(hoursAgo<24){
+    return `${hoursAgo}h ago`
+  }
+
+  return `${daysAgo}d ago`
+}
+
+const viewCount = (views) => {
+  const thousands = Math.floor(views / 1000);
+  if(views>=1000){
+    return `${thousands}k`
+  }
+  return views
+}
+
 const fetchData = async () => {
   try{
     const res = await fetch(forumLatest);
@@ -18,9 +64,35 @@ const fetchData = async () => {
 fetchData();
 
 const showLatestPosts = (data) => {
-  const { topic_list, users } = data;
-
-}
+  const { topic_list, users } = data; //use destructuring to get the topic_list and users properties from the data object.
+  const { topics } = topic_list;
+  postsContainer.innerHTML = topics.map((item)=>{
+   const {
+    id,
+    title,
+    views,
+    posts_count,
+    slug,
+    posters,
+    category_id,
+    bumped_at
+  } = item;
+  return `
+  <tr>
+    <td>
+      <p class='post-title'>${title}</p>
+    </td>
+    <td></td>
+    <td>${posts_count - 1}</td>
+    <td>      ${viewCount(views)}    </td>
+    <td>
+    ${timeAgo(bumped_at)}
+    </td>
+  </tr>
+  `;
+  })
+  .join('')
+};
 
 //?Line1 JavaScript is an asynchronous programming language. And this project will help you gain proficiency in asynchronous concepts. You'll code your own freeCodeCamp forum leaderboard. This project will cover the Fetch API, promises, Async/Await, and the try..catch statement.
 
@@ -44,3 +116,5 @@ const showLatestPosts = (data) => {
 // }
 
 //?7 You want to get the response body as a JSON object. The .json() method of your res variable returns a promise, which means you will need to await it.
+
+//?22 To display data in the Activity column, you need to use the bumped_at property of each topic, which is a timestamp in the ISO 8601 format. You need to process this data before you can show how much time has passed since a topic had any activity.
--- Learn/^02 JavaScript Algorithms and Data Structures/Cert Project/01 Palindrome Checker (Cert Projrect)/script.js
+++ Learn/^02 JavaScript Algorithms and Data Structures/Cert Project/01 Palindrome Checker (Cert Projrect)/script.js
@@ -11,10 +11,72 @@ checkButton.addEventListener('click', () => {
   }
 });
 
-const palCheck = (input) => {
+const cleanString = (input) =>{
   const lowercase = input.toLowerCase()
-  const half = lowercase.length/2
-  const inHalf = lowercase.split(lowercase[half])
   console.log(lowercase)
-  result.innerText = `${input} is a palindrome`
+
+  const cleanInput = lowercase.replaceAll(/_|\W/g,'')
+  console.log(cleanInput)
+  palCheck(cleanInput)
+}
+
+const palCheck = (cleanInput) => {
+
+
+
+
+  const inputArray = cleanInput.split('')
+  console.log(inputArray)
+
+  const half = Math.floor((inputArray.length)/2)
+  console.log(half)
+
+  const firstHalf = cleanInput.slice(0,half)
+  console.log(firstHalf)
+
+  const secondHalf = cleanInput.slice(half+1,inputArray.length)
+  console.log(secondHalf)
+
+  if(firstHalf==secondHalf){
+    result.innerText = `${cleanInput} is a palindrome`
+    console.log('True: ',cleanInput)
+  }else{
+    console.log('False: ',cleanInput)
+  }
+}
+
+// palCheck('A')
+// palCheck('eye')
+// palCheck('nope')
+
+//^----Tests----
+const tests = [
+  // 'A',
+  // 'eye',
+  // '_eye',
+  'race car',
+  // 'A man, a plan, a canal. Panama',
+  // 'never odd or even',
+  //  'My age is 0, 0 si ega ym.',
+  //  '0_0 (: /-\ :) 0-0]'
+  ]
+/*
+^true
+A
+eye
+_eye
+race car
+A man, a plan, a canal. Panama
+never odd or even
+ My age is 0, 0 si ega ym.
+ 0_0 (: /-\ :) 0-0
+^false
+not a palindrome
+nope
+almostomla
+1 eye for of 1 eye.
+five|\_/|four
+*/
+for(let i = 0;i<tests.length;i++){
+  cleanString(tests[i])
 }
