1aad60a593287d912196e1cc07a18550164b2d9a
AMESocker <aesocher@yahoo.com>
2024-03-05 13:24:50 -0800

    fixed rounding issue

--- Learn/^02 JavaScript Algorithms and Data Structures/Cert Project/14-04 Cash Register (Cert Project)/script.js
+++ Learn/^02 JavaScript Algorithms and Data Structures/Cert Project/14-04 Cash Register (Cert Project)/script.js
@@ -3,6 +3,17 @@ const purchaseBtn = document.getElementById('purchase-btn')
 const changeDue = document.getElementById('change-due')
 
 let price = 11;
+let cidDen = [
+  ["PENNY", 0.01],
+  ["NICKEL", 0.05],
+  ["DIME", 0.10],
+  ["QUARTER", 0.25],
+  ["ONE", 1.00],
+  ["FIVE", 5.00],
+  ["TEN", 10.00],
+  ["TWENTY", 20.00],
+  ["ONE HUNDRED", 100.00]
+]
 let cidA = [
   ["PENNY", 1.01],
   ["NICKEL", 2.05],
@@ -49,31 +60,64 @@ let cidD = [
 ];
 let cash = cashInput.value
 
+let display = []
 // console.log(cid[1][1])
-const cashValues = (changeDue,cid) => {
+
+
+const cashValues = (changeDue, cid) => {
+  let newChangeAmount = 0.00
+  newChangeAmount = changeDue
+  
   for (let i = 8; i >= 0; i--) {
-    console.log(cid[i][1]);
+  let numOfUnit = Math.floor(newChangeAmount / cidDen[i][1]);
+    let numOfUnitCid = (cid[i][1] / cidDen[i][1]);
     
+    // console.log(cidDen[i][0], newChangeAmount ,numOfUnit,numOfUnitCid ,cid[i][1])
+    // console.log(cidDen[i][0],numOfUnit,numOfUnitCid)
+    
+    // console.log(newChangeAmount,numOfUnit ,numOfUnitCid)
+    if (newChangeAmount > 0 && numOfUnit > 0 && numOfUnitCid >= 1) {
+      if(numOfUnit>=numOfUnitCid){
+        for (let j = 0; j < numOfUnitCid; j++) {
+          newChangeAmount = Math.round(newChangeAmount*100)/100-(cidDen[i][1])
+          // console.log(  Math.round(newChangeAmount*100)/100  )
+          console.log(cidDen[i][0])
+          // console.log('newAmount', newChangeAmount.toFixed(2))
+        }
+      }else{
+        for (let j = 0; j < numOfUnit; j++) {
+          newChangeAmount = Math.round(newChangeAmount*100)/100-(cidDen[i][1])
+          console.log(cidDen[i][0])
+
+          // console.log(cidDen[i][0], newChangeAmount.toFixed(2))
+        }
+      }
+      // console.log('Change Due:',newChangeAmount,'NOU',numOfUnit)
+    }
+    // console.log(cidDen[i][0],cid[i][1],cidDen[i][1]);
   }
+
+  // console.log(...display)
 }
-const registerTests = (price,cash,cid) => {
+
+const registerTests = (price, cash, cid) => {
   if (cash === price) {
     console.log('No change due')
     changeDue.textContent = 'No change due - customer paid with exact cash'
   } else if (cash < price) {
     console.log('Not enough money')
     // alert('Customer does not have enough money to purchase the item')
-  } else if ( cash > price){
-    let changeDue = cash-price
+  } else if (cash > price) {
+    let changeDue = cash - price
     let totalCid = 0
     for (let i = 0; i < 8; i++) {
       totalCid += cid[i][1]
     }
-    if(changeDue>totalCid){
+    if (changeDue > totalCid) {
       console.log('Status: INSUFFICIENT_FUNDS')
-    }else{
-      cashValues(changeDue,cid)
-      console.log('Change due:',changeDue,'CID:',totalCid)
+    } else {
+      cashValues(changeDue, cid)
+      console.log('Change due:', changeDue, 'CID:', totalCid)
     }
 
   }
@@ -88,16 +132,16 @@ const registerTests = (price,cash,cid) => {
 // })
 
 const tests = [//(price,cash)
-  registerTests(1,1),        //No change due
+  // registerTests(1,1),        //No change due
   registerTests(11,1),      //Not enough money
 
   registerTests(19.5,20,cidA),  //Status: OPEN QUARTER: $0.5
-  registerTests(3.26,100,cidA),//Status: OPEN TWENTY: $60 TEN: $20 FIVE: $15 ONE: $1 QUARTER: $0.5 DIME: $0.2 PENNY: $0.04
+  // registerTests(3.26, 100, cidA),//Status: OPEN TWENTY: $60 TEN: $20 FIVE: $15 ONE: $1 QUARTER: $0.5 DIME: $0.2 PENNY: $0.04
   // registerTests(19.5,20,cidB),
   // registerTests(19.5,20,cidC),
   // registerTests(19.5,20,cidD),
 ]
-for(let i = 0;i<tests.length;i++){
+for (let i = 0; i < tests.length; i++) {
   tests[i]
 }
 
@@ -124,7 +168,8 @@ When price is 19.5, the value in the #cash element is 20, cidA is
 ["TEN", 20], 
 ["TWENTY", 60], 
 ["ONE HUNDRED", 100]]
-,and the #purchase-btn element is clicked, the value in the #change-due element should be Status: OPEN QUARTER: $0.5
+,and the #purchase-btn element is clicked, the value in the #change-due element should be 
+'Status: OPEN QUARTER: $0.5'
 
 When price is 3.26, the value in the #cash element is 100, cidA is 
 [["PENNY", 1.01], 
---
+++ Learn/^02 JavaScript Algorithms and Data Structures/Cert Project/16-05 Pokemon Search App (Cert Project)/script.js
@@ -0,0 +1,31 @@
+
+
+
+//^----Tests----
+/*
+When the 
+#search-input element contains the value 
+Red and the #search-button element is clicked, an 
+alert should appear with the text Pokémon not found
+
+When the 
+#search-input element contains the value 
+Pikachu and the #search-button element is clicked, the values in the 
+#pokemon-name, #pokemon-id, #weight,          #height,       #hp, #attack, #defense, #special-attack, #special-defense, and #speed elements should be 
+PIKACHU,       #25 or 25,   Weight: 60 or 60, Height: 4 or 4, 35, 55,       40,       50,             50, and               90, respectively
+
+When the #search-input element contains the value 
+Pikachu and the #search-button element is clicked, you should add an img element with the id of sprite and the src set to the Pokémon's front_default sprite to the page
+
+When the #search-input element contains the value 
+Pikachu and the #search-button element is clicked, the 
+#types element should contain a single inner element with the value 
+ELECTRIC. The #types element content should be cleared between searches
+
+When the #search-input element contains the value 
+94 and the #search-button element is clicked, the values in the #pokemon-name, #pokemon-id, #weight, #height, #hp, #attack, #defense, #special-attack, #special-defense, and #speedelements should be GENGAR, #94 or 94, Weight: 405 or 405, Height: 15 or 15, 60, 65, 60, 130, 75, and 110, respectively
+
+When the #search-input element contains the value 94 and the #search-button element is clicked, you should add an img element with the id of sprite and the src set to the Pokémon's front_default sprite to the page
+
+When the #search-input element contains the value 94 and the #search-button element is clicked, the #types element should contain two inner elements with the text values GHOST and POISON, respectively. The #types element content should be cleared between searches
+*/
