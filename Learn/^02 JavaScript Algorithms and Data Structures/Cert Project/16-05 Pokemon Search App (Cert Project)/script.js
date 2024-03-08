1571f53cae50a89009658b5b23f6c6195db9f12c
AMESocker <aesocher@yahoo.com>
2024-03-07 20:10:45 -0800

    Completed Pokemon Search App

--- Learn/^02 JavaScript Algorithms and Data Structures/Cert Project/16-05 Pokemon Search App (Cert Project)/script.js
+++ Learn/^02 JavaScript Algorithms and Data Structures/Cert Project/16-05 Pokemon Search App (Cert Project)/script.js
@@ -11,97 +11,91 @@ const defense = document.getElementById("defense");
 const specialAttack = document.getElementById("special-attack");
 const specialDefense = document.getElementById("special-defense");
 const speed = document.getElementById("speed");
+const img = document.getElementById('img')
 
 // const pokemonAPI = 'https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/';
-const pokemonAPI = 'https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/metapod';
-
-const fetchData = async () => {
+const cleanData = (input) => {
+  let pokLower = input.toLowerCase()
+  const pokemonAPI = `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokLower}`;
+  // console.log(pokLower, pokemonAPI)
+  fetchData(pokemonAPI)
+}
+const fetchData = async (pokemon) => {
   try{
-    const res = await fetch(pokemonAPI);
+    const res = await fetch(pokemon);
     const data = await res.json();
-    console.log(data)
-  }catch(err){
-    console.log(err)
+        console.log('Yes')
+        displayData(data)
+
+    }catch(err){
+      alert('Pokémon not found')
+    // console.log(err)
+    // displayData(err)
   }
 };
-let metapodEx =
-{
-  "base_experience": 72,
-  "height": 7,
-  "id": 11,
-  "name": "metapod",
-  "order": 15,
-  "sprites": {
-      "back_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/11.png",
-      "back_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/11.png",
-      "front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/11.png",
-      "front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/11.png"
-  },
-  "stats": [
-      {
-          "base_stat": 50,
-          "effort": 0,
-          "stat": {
-              "name": "hp",
-              "url": "https://pokeapi.co/api/v2/stat/1/"
-          }
-      },
-      {
-          "base_stat": 20,
-          "effort": 0,
-          "stat": {
-              "name": "attack",
-              "url": "https://pokeapi.co/api/v2/stat/2/"
-          }
-      },
-      {
-          "base_stat": 55,
-          "effort": 2,
-          "stat": {
-              "name": "defense",
-              "url": "https://pokeapi.co/api/v2/stat/3/"
-          }
-      },
-      {
-          "base_stat": 25,
-          "effort": 0,
-          "stat": {
-              "name": "special-attack",
-              "url": "https://pokeapi.co/api/v2/stat/4/"
-          }
-      },
-      {
-          "base_stat": 25,
-          "effort": 0,
-          "stat": {
-              "name": "special-defense",
-              "url": "https://pokeapi.co/api/v2/stat/5/"
-          }
-      },
-      {
-          "base_stat": 30,
-          "effort": 0,
-          "stat": {
-              "name": "speed",
-              "url": "https://pokeapi.co/api/v2/stat/6/"
-          }
-      }
-  ],
-  "types": [
-      {
-          "slot": 1,
-          "type": {
-              "name": "bug",
-              "url": "https://pokeapi.co/api/v2/type/7/"
-          }
-      }
-  ],
-  "weight": 99
+
+const displayData = (data) => {
+ 
+    console.log(data)
+pokemonName.textContent += data.name.toUpperCase()
+pokemonId.textContent +=   data.id
+weight.textContent += data.weight
+height.textContent += data.height
+hp.textContent += data.stats[0].base_stat
+attack.textContent += data.stats[1].base_stat
+defense.textContent += data.stats[2].base_stat
+specialAttack.textContent += data.stats[3].base_stat
+specialDefense.textContent += data.stats[4].base_stat
+speed.textContent += data.stats[5].base_stat
+
+let dataSprite = data.sprites.front_default
+img.innerHTML += `
+<img id='sprite' src=${dataSprite}>`
+let dataType = data.types[0].type.name.toUpperCase()
+
+types.innerHTML +=
+`<div>${dataType}</div>
+`
+if(data.types[1]){
+    let dataType1 = data.types[1].type.name.toUpperCase()
+    types.innerHTML +=
+`
+<div>${dataType1}</div>
+`
 }
-fetchData();
+  
+}
+
+searchButton.addEventListener('click',()=>{
+    let elements = [
+        pokemonName,
+        pokemonId,
+        weight,
+        height,
+        hp,
+        attack,
+        defense,
+        specialAttack,
+        specialDefense,
+        speed,
+    ]
+for (let i = 0; i < elements.length; i++) {
+    elements[i].textContent = ''
+}
+types.innerHTML =''
+img.innerHTML = ''
+    cleanData(searchInput.value)
+})
+
 //^----Tests----
-
-
+const tests = [
+//   'Red',
+//   'Pikachu',
+//   '94'
+]
+for(let i = 0;i<tests.length;i++){
+  cleanData(tests[i])
+}
 
 /*PokéAPI Proxy
 Usage
@@ -136,8 +130,18 @@ alert should appear with the text Pokémon not found
 When the 
 #search-input element contains the value 
 Pikachu and the #search-button element is clicked, the values in the 
-#pokemon-name, #pokemon-id, #weight,          #height,       #hp, #attack, #defense, #special-attack, #special-defense, and #speed elements should be 
-PIKACHU,       #25 or 25,   Weight: 60 or 60, Height: 4 or 4, 35, 55,       40,       50,             50, and               90, respectively
+#pokemon-name,    PIKACHU,
+#pokemon-id,      #25 or 25
+#weight,          Weight: 60 or 60     
+#height,          Height: 4 or 4
+#hp,              35
+#attack,          55
+#defense,         40
+#special-attack,  50 
+#special-defense, 50 
+#speed,           90
+elements should be 
+
 
 When the #search-input element contains the value 
 Pikachu and the #search-button element is clicked, you should add an img element with the id of sprite and the src set to the Pokémon's front_default sprite to the page
@@ -150,7 +154,9 @@ ELECTRIC. The #types element content should be cleared between searches
 When the #search-input element contains the value 
 94 and the #search-button element is clicked, the values in the #pokemon-name, #pokemon-id, #weight, #height, #hp, #attack, #defense, #special-attack, #special-defense, and #speedelements should be GENGAR, #94 or 94, Weight: 405 or 405, Height: 15 or 15, 60, 65, 60, 130, 75, and 110, respectively
 
-When the #search-input element contains the value 94 and the #search-button element is clicked, you should add an img element with the id of sprite and the src set to the Pokémon's front_default sprite to the page
+When the #search-input element contains the value 
+94 and the #search-button element is clicked, you should add an img element with the id of sprite and the src set to the Pokémon's front_default sprite to the page
 
-When the #search-input element contains the value 94 and the #search-button element is clicked, the #types element should contain two inner elements with the text values GHOST and POISON, respectively. The #types element content should be cleared between searches
+When the #search-input element contains the value 
+94 and the #search-button element is clicked, the #types element should contain two inner elements with the text values GHOST and POISON, respectively. The #types element content should be cleared between searches
 */
