8675d125d606a501e5d54c97445a8d689b4a7214
AMESocker <aesocher@yahoo.com>
2024-03-11 16:27:05 -0700

    Completed 8.6.17

--- Learn/^08 Python Scientific Computing/05 Password Generator/main.py
+++ Learn/^08 Python Scientific Computing/05 Password Generator/main.py
@@ -25,8 +25,12 @@ def generate_password(length,nums, special_chars, uppercase, lowercase):
             (special_chars,fr'[{symbols}]')
             ]
         # Check constraints
+        count = 0
         for constraint, pattern in constraints:
-            len(re.findall(pattern, password))
+            if constraint <= len(re.findall(pattern, password)):
+                count += 1
+        if count == 4:
+            break
     return password
 
 # new_password = generate_password(8)
