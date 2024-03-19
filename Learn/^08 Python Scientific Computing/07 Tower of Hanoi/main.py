963fa11fb970e5268c3cf03743f61ef56b1dd4c3
AMESocker <aesocher@yahoo.com>
2024-03-19 11:20:28 -0700

    Completed 8.7.12

---
+++ Learn/^08 Python Scientific Computing/07 Tower of Hanoi/main.py
@@ -0,0 +1,14 @@
+NUMBER_OF_DISKS = 3
+number_of_moves = 2**NUMBER_OF_DISKS - 1
+# print(number_of_moves)
+rods = {
+  'A':list(range(3, 0, -1)),
+  'B':[],
+  'C':[],  
+}
+
+def move():
+  print(rods)
+
+move()
+# print(type(rods['A']))
