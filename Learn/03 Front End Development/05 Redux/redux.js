84b5ee8659dcb37eb8e23a14bb226a54b4f4a913
AMESocker <aesocher@yahoo.com>
2024-05-17 07:53:53 -0700

    Completed 3.5.8

--- Learn/03 Front End Development/05 Redux/redux.js
+++ Learn/03 Front End Development/05 Redux/redux.js
@@ -7,7 +7,7 @@ const reducer = (state = 5) => {
 // For example: Redux.createStore()
 // Define the store here:
 
-let store = Redux.createStore(reducer);
+//^let store = Redux.createStore(reducer);
 */
 /* //? 2 Get State from the Redux Store
 const store = Redux.createStore(
@@ -15,7 +15,7 @@ const store = Redux.createStore(
 );
 
 // Change code below this line
-let currentState = store.getState(); */
+^let currentState = store.getState(); */
 /* //? 3 Define a Redux Action
 // Define an action here:
 const action = {
@@ -29,7 +29,7 @@ const action = {
 function actionCreator() {
   return action;
 } */
-//? 5 Dispatch an Action Event
+/* //? 5 Dispatch an Action Event
 const store = Redux.createStore(
   (state = {login: false}) => state
 );
@@ -41,4 +41,103 @@ const loginAction = () => {
 };
 
 // Dispatch the action here:
-store.dispatch(loginAction());
+store.dispatch(loginAction()); */
+/* //? 6 Handle an Action in the Store
+const defaultState = {
+  login: false
+};
+
+const reducer = (state = defaultState, action) => {
+  // Change code below this line
+  if (action.type === 'LOGIN') {
+    return {
+      login: true
+    }
+  } else {
+    return state;
+  }
+  // Change code above this line
+};
+
+const store = Redux.createStore(reducer);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN'
+  }
+}; */
+/* //? 7 Use a Switch Statement to Handle Multiple Actions
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+  // Change code below this line
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        authenticated: true
+      }
+    case 'LOGOUT':
+      return {
+        authenticated: false
+      }
+    default:
+      return state;
+  }
+  // Change code above this line
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: 'LOGOUT'
+  }
+}; */
+//? 8 Use const for Action Types
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
+
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+
+  switch (action.type) {
+    case LOGIN: 
+      return {
+        authenticated: true
+      }
+    case LOGOUT: 
+      return {
+        authenticated: false
+      }
+
+    default:
+      return state;
+
+  }
+
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: LOGIN
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: LOGOUT
+  }
+};
