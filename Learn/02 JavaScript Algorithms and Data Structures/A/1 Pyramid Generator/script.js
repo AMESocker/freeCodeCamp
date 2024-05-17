e991e74ac89f47a78958bf5f8b8a9e5cbcb66de8
AMESocker <aesocher@yahoo.com>
2024-05-17 10:54:20 -0700

    Completed 2.A.1.29

--- Learn/02 JavaScript Algorithms and Data Structures/A/1 Pyramid Generator/script.js
+++ Learn/02 JavaScript Algorithms and Data Structures/A/1 Pyramid Generator/script.js
@@ -1,6 +1,17 @@
-let character = 'Hello';
-let count = 8;
+const character = "#";
+const count = 8;
+const rows = [];
+for (let i = 0; "condition"; "iteration") {}
 
+// let rows = ["Naomi", "Quincy","CamperChan"];
+// let pushed = rows.push("freeCodeCamp");
+// console.log(pushed);
+// let popped = rows.pop();
+// console.log(popped);
+// console.log(rows);
+// console.log(rows[0]);
+// rows[rows.length-1] = 10;
+// console.log(count+1)
 // console.log(character)
 // character = "World"
 // console.log(character)
