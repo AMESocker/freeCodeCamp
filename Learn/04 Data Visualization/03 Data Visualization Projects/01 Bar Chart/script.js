db70364715070107d9f68fcc0dbf899c6a030e61
AMESocker <aesocher@yahoo.com>
2024-05-23 09:20:48 -0700

    Added barWidth

--- Learn/04 Data Visualization/03 Data Visualization Projects/01 Bar Chart/script.js
+++ Learn/04 Data Visualization/03 Data Visualization Projects/01 Bar Chart/script.js
@@ -1,8 +1,36 @@
+const chartDiv = document.getElementById('chart')
 const dataset = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
 
+fetch(dataset)
+  .then(response => response.json())
+  .then(data => {
+    displayData(data.data)
+  })
 
-const req = new XMLHttpRequest();
-req.open('GET', dataset, true);
-req.send();
+const displayData = (dataResponse) => {
+  const dataset = dataResponse;
 
+  const w = 900;
+  const h = 500;
+  const barWidth = w / dataset.length;
+
+  const svg = d3.select(chartDiv)
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .attr("class", "bar-chart")
+
+  svg.selectAll('rect')
+    .data(dataset)
+    .enter()
+    .append('rect')
+    .attr("x", (d, i) => i * barWidth)
+    .attr("y", (d) => h - d[1] / 40)
+    .attr("width", barWidth)
+    .attr("height", (d) => d[1])
+    .attr("class", "bar")
+
+
+
+}
 
