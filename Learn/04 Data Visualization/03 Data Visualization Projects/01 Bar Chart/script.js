0f1c4f9ce64c303e23dbec56c542fd5d7ae867ad
AMESocker <aesocher@yahoo.com>
2024-05-24 10:56:58 -0700

    added tooltip, scale and new colors

--- Learn/04 Data Visualization/03 Data Visualization Projects/01 Bar Chart/script.js
+++ Learn/04 Data Visualization/03 Data Visualization Projects/01 Bar Chart/script.js
@@ -10,9 +10,24 @@ fetch(dataset)
 const displayData = (dataResponse) => {
   const dataset = dataResponse;
 
+  //get year from dataset
+  const getYear = (date) => {
+    return date.substring(0, 4)
+  }
+  dataset.forEach((d) => {
+    d[0] = getYear(d[0])
+  })
+  console.log(dataset)
+ 
+  
   const w = 900;
   const h = 500;
-  const barWidth = w / dataset.length;
+  const padding = 20;
+  const barWidth = (w-padding) / dataset.length;
+
+  const xScale = d3.scaleLinear()
+    .domain([d3.min(dataset,(d)=>d[0]), d3.max(dataset,(d)=>d[0])])
+    .range([0, w - padding]);
 
   const svg = d3.select(chartDiv)
     .append("svg")
@@ -26,10 +41,20 @@ const displayData = (dataResponse) => {
     .append('rect')
     .attr("x", (d, i) => i * barWidth)
     .attr("y", (d) => h - d[1] / 40)
-    .attr("width", barWidth)
+    .attr("width", (barWidth))
     .attr("height", (d) => d[1])
     .attr("class", "bar")
+    .attr("data-date", (d) => d[0])
+    .attr("data-gdp", (d) => d[1])
+    .append('title')
+    .attr('id', 'tooltip')
+    .text((d) => d[0])
 
+const xAxis = d3.axisBottom(xScale);
+
+svg.append("g")
+    .attr("transform", "translate(0," + (h - padding) + ")")
+    .call(xAxis);
 
 
 }
