26904919a785533a156cb760dcd1de1f463b5c05
AMESocker <aesocher@yahoo.com>
2024-06-14 07:50:38 -0700

    redirect function

--- Learn/06 Back End Development and APIs/4 Back End Development and APIs Projects/3 URL Shortener/index.js
+++ Learn/06 Back End Development and APIs/4 Back End Development and APIs Projects/3 URL Shortener/index.js
@@ -11,11 +11,11 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', function(req, res) {
+res.sendFile(process.cwd() + '/views/index.html');
+});
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
@@ -33,20 +33,25 @@ const randomHexNumber = () => {
   hexNumber = hexNumber.join('')
   return hexNumber
 }
-console.log(randomHexNumber())
 
 app.post('/api/shorturl/', function(req, res) {
   const url = req.body.url;
   console.log(url)
+
   const urlRegex = /^(http|https):\/\/[^ "]+$/;
-  if (!urlRegex.test(url)) {
-    res.json({ error: 'invalid URL' },{url: url});
-    return;
+  // if (!urlRegex.test(url)) {
+  //   res.json({ error: 'invalid URL' },{url: url});
+  //   return;
+  // }
+console.log(shortUrls.find(url => url.original_url === url.original_url))
+
+  if (!shortUrls.find(urlData => urlData.original_url === url)) {
+    shortUrls.push({
+      original_url: url,
+      short_url: randomHexNumber(),
+    });
   }
-  shortUrls.push({
-    original_url: url,
-    short_url: randomHexNumber(),
-  });
+
   res.json({ 
     original_url: url, 
     "short_url":"/api/shorturl/" + shortUrls[shortUrls.length - 1].short_url,
@@ -59,7 +64,7 @@ app.get('/api/shorturl/:short_url', function(req, res) {
   const url = shortUrls.find(url => url.short_url === short_url);
   console.log(url,res.redirect)
   if (url) {
-    res.redirect
+    res.redirect(url.original_url)
   } else {
     res.json({ error: 'No short URL found for the given input' });
   }
