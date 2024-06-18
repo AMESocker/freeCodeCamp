5234f38bf764363cf10e6ecc698ed0f36be4661f
AMESocker <aesocher@yahoo.com>
2024-06-18 09:56:58 -0700

    Completed URL Tests

--- Learn/06 Back End Development and APIs/4 Back End Development and APIs Projects/3 URL Shortener/index.js
+++ Learn/06 Back End Development and APIs/4 Back End Development and APIs Projects/3 URL Shortener/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser')
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,15 +11,19 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
-
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', function(req, res) {
-res.sendFile(process.cwd() + '/views/index.html');
+app.use(function (req, res, next) {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
@@ -31,40 +36,46 @@ const randomHexNumber = () => {
     hexNumber.push(random)
   }
   hexNumber = hexNumber.join('')
-  return hexNumber
+  return hexNumber.toString()
 }
 
-app.post('/api/shorturl/', function(req, res) {
+app.post('/api/shorturl/', function (req, res) {
+  // console.log('app.post - /api/shorturl/')
+
   const url = req.body.url;
-  console.log(url)
+  const urlObj = new URL(url);
+  console.log(urlObj)
 
-  const urlRegex = /^(http|https):\/\/[^ "]+$/;
-  // if (!urlRegex.test(url)) {
-  //   res.json({ error: 'invalid URL' },{url: url});
-  //   return;
-  // }
-console.log(shortUrls.find(url => url.original_url === url.original_url))
+  dns.lookup(urlObj.hostname, (err) => {
+    if (err) {
+      res.json({ error: 'invalid url' });
+    } else if (!urlObj.protocol.includes('http')) {
+      res.json({ error: 'invalid url' });
+    } else {
+      if (!shortUrls.find(urlData => urlData.original_url === url)) {
+        shortUrls.push({
+          original_url: urlObj,
+          short_url: randomHexNumber(),
+        });
+      }
 
-  if (!shortUrls.find(urlData => urlData.original_url === url)) {
-    shortUrls.push({
-      original_url: url,
-      short_url: randomHexNumber(),
-    });
-  }
-
-  res.json({ 
-    original_url: url, 
-    "short_url":"/api/shorturl/" + shortUrls[shortUrls.length - 1].short_url,
+      res.json({
+        original_url: url,
+        "short_url": shortUrls[shortUrls.length - 1].short_url,
+      });
+    }
   });
-  console.log(shortUrls)
+
+
+
 })
 
-app.get('/api/shorturl/:short_url', function(req, res) {
+app.get('/api/shorturl/:short_url', function (req, res) {
+  console.log('app.get')
   const short_url = req.params.short_url;
   const url = shortUrls.find(url => url.short_url === short_url);
-  console.log(url,res.redirect)
   if (url) {
-    res.redirect(url.original_url)
+    res.redirect(url.original_url.href)
   } else {
     res.json({ error: 'No short URL found for the given input' });
   }
@@ -72,6 +83,7 @@ app.get('/api/shorturl/:short_url', function(req, res) {
 
 
 
-app.listen(port, function() {
+
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
