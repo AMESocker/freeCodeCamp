488b007d5e9bace2d2fccf602b10e650b1cfdd66
AMESocker <aesocher@yahoo.com>
2024-06-21 09:46:52 -0700

    Completed Tests File Metadata

--- Learn/06 Back End Development and APIs/4 Back End Development and APIs Projects/5 File Metadata/index.js
+++ Learn/06 Back End Development and APIs/4 Back End Development and APIs Projects/5 File Metadata/index.js
@@ -4,24 +4,31 @@ require('dotenv').config()
 const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-// app.use(multer());
+
+app.use(function (req, res, next) {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+  next();
+});
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/api/fileanalyse',upload.single('jpg'), function (req, res, next) {
+app.post('/api/fileanalyse',upload.single('upfile'), function (req, res, next) {
   console.log(req.file, req.body)
-  // res.json({
-  //   // name: req.file.originalname,
-  //   type: req.file.mimetype,
-  //   size: req.file.size
-  // });
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 });
+
+
+
 
 
 const port = process.env.PORT || 3000;
