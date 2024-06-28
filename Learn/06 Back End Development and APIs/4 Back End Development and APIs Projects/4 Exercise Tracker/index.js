367bfc160637d831b46cdb300a50c8eaa0503ed8
AMESocker <aesocher@yahoo.com>
2024-06-28 09:17:49 -0700

    added mongoose

--- Learn/06 Back End Development and APIs/4 Back End Development and APIs Projects/4 Exercise Tracker/index.js
+++ Learn/06 Back End Development and APIs/4 Back End Development and APIs Projects/4 Exercise Tracker/index.js
@@ -3,91 +3,146 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Schema = mongoose.Schema;
+
+//----Middleware----
 app.use(cors())
 app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
 app.use(bodyParser.urlencoded({ extended: false }))
-
 app.use(function (req, res, next) {
-  console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+  // console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+  console.log(req.method + ' ' + req.path);
   next();
 });
 
-let users = [];
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+  });
 
-app.post('/api/users', (req, res) => {
+//----Date Format----
+let dateEntry;
+let month =
+  new Date().getMonth() + 1 < 10
+    ? "0" + (new Date().getMonth() + 1)
+    : new Date().getMonth() + 1;
+dateEntry =
+  new Date().getFullYear() + "-" + month + "-" + new Date().getDate();
+
+//----Mongoose Schema----
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  count: { type: Number, default: 0 },
+  log: [
+    {
+      description: { type: String, required: true },
+      duration: { type: Number, required: true },
+      date: { type: Date, default: dateEntry }
+    }
+  ]
+});
+
+let User = mongoose.model('User', userSchema);
+
+
+//----Routes----
+app.post("/api/users", (req, res) => {
   const newUser = req.body.username;
-  const id = users.length;
-  if (!users.find(user => user.username === newUser)){
-    // res.json({ error: 'Username already taken' });
-    users.push({
-      _id : id,
-      username: newUser
-    });
-    
-    res.json({
-      _id : id,
-      username: newUser
-    });
-  }
-    // console.log(users);
-})
+  const user = new User({ username: newUser });
+
+  User.findOne({ username: newUser }, function (err, isUser) {
+    if (err) return console.error(err);
+    if (isUser) {
+      res.json({
+        _id: isUser._id,
+        username: isUser.username,
+      });
+    } else {
+      user.save((err, data) => {
+        if (err) return console.error(err);
+        res.json({
+          _id: data._id,
+          username: data.username
+        });
+      });
+    }
+  });
+});
 
 app.get('/api/users', (req, res) => {
-  res.json(users);
-})
+  User.find({}, (err, data) => {
+    if (err) return console.error(err);
+    res.json(data);
+  });
+});
 
-let log = [];
+app.post("/api/users/:_id/exercises", (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.params._id },
+    { $push: { log: req.body } },
+    { new: true },
+    (err, data) => {
+      if (err) return console.error(err);
+      if (data) {
+        month =
+          new Date(req.body.date).getMonth() + 1 < 10
+            ? "0" + (new Date(req.body.date).getMonth() + 1)
+            : new Date(req.body.date).getMonth() + 1;
+        dateEntry =
+          new Date(req.body.date).getFullYear() +
+          "-" +
+          month +
+          "-" +
+          new Date(req.body.date).getDate();
 
-app.post('/api/users/:_id/exercises', (req, res) => {
+      res.json({
+        username: data.username,
+        _id: data._id,
+        date: dateEntry,
+        duration: parseInt(req.body.duration),
+        description: req.body.description,
+      });
+    }
+    }
+  );
+});
 
-  const userId = req.params._id;
-  const user = users.find(user => user._id == userId);
-  if (!user) {
-    res.json({ error: 'User not found' });
-  }
-  const description = req.body.description;
-  const duration = req.body.duration;
-  const date = req.body.date;
+app.get('/api/users/:_id/logs', (req, res) => {
+  let _id = req.params._id;
+  User.findById(_id, (err, data) => {
+    if (err) return console.error(err);
 
-  let userPresent = log.find(log => log._id == userId)
+    let from = new Date(req.query.from);
+    let to = new Date(req.query.to);
+    let limit = parseInt(req.query.limit);
 
-  const newExercise = {
-    username: user.username,
-    count: userPresent ? userPresent.log.length + 1 : 1,
-    _id: userId,
-    log:[{
-      description: description,
-      duration: Number(duration),
-      date:  date ? date : 
-      new Intl.DateTimeFormat('en-US', { day: '2-digit', month: '2-digit', year: 'numeric' }).format(new Date()),
-      // new Date().toDateString(),
-    }]
-  }
+    if (!isNaN(from.valueOf())) {
+      data.log = data.log.filter((session) => new Date(session.date) >= from);
+    }
+    if (!isNaN(to.valueOf())) {
+      data.log = data.log.filter((session) => new Date(session.date) <= to);
+    }
+    if (!isNaN(limit)) {
+      data.log = data.log.slice(0, limit);
+    }
 
-  // const date = new Date();
-  // const formatter = new Intl.DateTimeFormat('en-US', { day: '2-digit', month: '2-digit', year: 'numeric' });
-  // const formattedDate = new Intl.DateTimeFormat('en-US', { day: '2-digit', month: '2-digit', year: 'numeric' }).format(new Date());
+    console.log(data.log[0].date.toDateString());
+    res.json({
+      _id: data._id,
+      username: data.username,
+      count: data.log.length,
+      log: data.log.map((session) => ({
+        description: session.description,
+        duration: session.duration,
+        date: session.date.toDateString(),
+      })),
+    });
 
-
-  if(userPresent){
-    userPresent.count = userPresent.log.length + 1;
-    userPresent.log.push(newExercise.log[0]);
-    res.json({newExercise});
-  } else {
-    log.push(newExercise);
-    res.json({newExercise});
-  }
-
-})
-
-app.get('/api/users/:_id/logs?', (req, res) => {
-  res.json(log);
-  console.log(log);
-})
+  })
+});
 
 
 
