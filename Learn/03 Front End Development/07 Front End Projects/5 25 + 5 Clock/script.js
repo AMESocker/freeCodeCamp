88aaf15231e7799f91748ef6637e9465af1bebe1
AMESocker <aesocher@yahoo.com>
2024-07-11 19:20:14 -0700

    converted time to seconds, added countdownType function

--- Learn/03 Front End Development/07 Front End Projects/5 25 + 5 Clock/script.js
+++ Learn/03 Front End Development/07 Front End Projects/5 25 + 5 Clock/script.js
@@ -1,4 +1,4 @@
-let intervalID
+let countdownID
 class Controls extends React.Component {
   constructor(props) {
     super(props);
@@ -25,11 +25,11 @@ class Clock extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      breakLength: 5,
-      sessionLength: 25,
-      timeLeft: 25*60,
+      breakLength: 300,
+      sessionLength: 1500,
       timerState: 'stopped',
       timerType: 'Session',
+      timeLeft: 1500,
       intervalID: '',
     }
 
@@ -39,46 +39,48 @@ class Clock extends React.Component {
     this.runClock = this.runClock.bind(this);
   }
 
-  incrementBreak = () => {
-    if (this.state.breakLength < 60) {
+  incrementBreak = () => { 
+    if (this.state.breakLength < 3600) {
       this.setState({
-        breakLength: this.state.breakLength + 1
+        breakLength: this.state.breakLength + 60
       })
     }
   }
 
-  decrementBreak = () => {
-    if (this.state.breakLength > 1) {
+  decrementBreak = () => { 
+    if (this.state.breakLength > 60) {
       this.setState({
-        breakLength: this.state.breakLength - 1
+        breakLength: this.state.breakLength - 60
       })
     }
   }
 
   incrementSession = () => {
-    if (this.state.sessionLength < 60) {
+    if (this.state.sessionLength < 3600) {
       this.setState({
-        sessionLength: this.state.sessionLength + 1,
-        timeLeft: this.state.sessionLength*60 + 1,
+        sessionLength: this.state.sessionLength + 60,
+        timeLeft: this.state.timeLeft + 60,
       })
     }
   }
 
   decrementSession = () => {
-    if (this.state.sessionLength > 1) {
+    if (this.state.sessionLength > 60) {
       this.setState({
-        sessionLength: this.state.sessionLength - 1
+        sessionLength: this.state.sessionLength - 60,
+        timeLeft: this.state.timeLeft - 60,
       })
     }
   }
 
   resetClock = () => {
+    clearInterval(countdownID);
     this.setState({
-      breakLength: 5,
-      sessionLength: 25,
-      timeLeft: 25 * 60,
+      breakLength: 300,
+      sessionLength: 1500,
       timerState: 'stopped',
       timerType: 'Session',
+      timeLeft: 1500,
       intervalID: '',
     })
 
@@ -86,24 +88,48 @@ class Clock extends React.Component {
 
   runClock = () => {
     if (this.state.timerState === 'stopped') {
-      intervalID = setInterval(() => {
+      countdownID = setInterval(() => {
         this.setState({
           timeLeft: this.state.timeLeft - 1,
           timerState: 'running',
         })
+        this.countdownType();
       }, 1000);
+    // }, 100);//for testing
     } else if (this.state.timerState === 'running') {
-      clearInterval(intervalID);
+      clearInterval(countdownID);
       this.setState({
         timerState: 'stopped',
       })
     }
+    
   }
 
+  countdownType = () => {
+    if(this.state.timeLeft === 0){console.log('timeLeft is 0')
+      document.getElementById('beep').play();
+      clearInterval(countdownID);
+      if(this.state.timerType === 'Session'){
+        this.setState({
+          timerState: 'stopped',
+          timerType: 'Break',
+          timeLeft: this.state.breakLength,
+        })
+        this.runClock();
+      } else {
+        this.setState({
+          timerState: 'stopped',
+          timerType: 'Session',
+          timeLeft: this.state.sessionLength,
+        })
+        this.runClock();
+      }
+    }
+  }
 
   timeDisplay = () => {
     let minutes = Math.floor(this.state.timeLeft / 60);
-    let seconds = this.state.timeLeft - 60 * minutes;
+    let seconds = this.state.timeLeft - minutes*60;
     return seconds = seconds < 10 ? "0" + seconds : seconds, minutes = minutes < 10 ? "0" + minutes : minutes,
     minutes + ":" + seconds
   }
@@ -112,18 +138,25 @@ class Clock extends React.Component {
     return (
       <div id="clock" class="container text-center">
           <h1 class="col-12">25 + 5 Clock</h1>
-        <Controls />
-        <div class="col-12" id="break-length">{this.state.breakLength}</div>
+          <div id="break-label">Break Length</div>
+        <div class="length row">
+          <div class="col-3"></div>
+          <button class="time col-2" id="break-decrement" onClick={this.decrementBreak}>-</button>
+          <div id="break-length" class="col-2">{this.state.breakLength/60}</div>
+          <button class="time col-2" id="break-increment" onClick={this.incrementBreak}>+</button>
+          <div class="col-3"></div>
+        </div>
+
 
         <div id="session-label">Session Length</div>
         <div class="length row">
           <div class="col-3"></div>
           <button class="time col-2" id="session-decrement" onClick={this.decrementSession}>-</button>
-          <div class="col-2" id="session-length">{this.state.sessionLength}</div>
+          <div class="col-2" id="session-length">{Math.floor(this.state.sessionLength/60)}</div>
           <button class="time col-2" id="session-increment" onClick={this.incrementSession}>+</button>
           <div class="col-3"></div>
         </div>
-        <div id="timer-label">Session</div>
+        <div id="timer-label">{this.state.timerType}</div>
         <div id="time-left">{this.timeDisplay()}</div>
         <div class="row">
           <div class="col-2"></div>
@@ -133,7 +166,8 @@ class Clock extends React.Component {
           <div class="col-2"></div>
         </div>
 
-        <audio id="beep" src="https://www.soundjay.com/buttons/sounds/beep-02.mp3"></audio>
+        {/* <audio id="beep" src="https://www.soundjay.com/buttons/sounds/beep-02.mp3"></audio> */}
+        <audio id="beep" src="https://www.soundjay.com/misc/sounds/magic-chime-01.mp3"></audio>
       </div>
     )
   }
